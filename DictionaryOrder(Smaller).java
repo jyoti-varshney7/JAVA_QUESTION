aaab7815051cc7a7062368c648349b6b4a77a62d
Jyoti Varshney <jyoti.varshney_cs18@gla.ac.in>
2021-07-14 01:53:09 +0530

    Create DictionaryOrder(Smaller).java

---
+++ DictionaryOrder(Smaller).java
@@ -0,0 +1,53 @@
+// Take as input str, a string. Write a recursive function which returns all the words possible by rearranging the characters of this string which are in dictionary order smaller than the given string. The output strings must be lexicographically sorted.
+
+// Input Format
+// Single line input containing a string
+
+// Constraints
+// Length of string <= 25
+
+// Output Format
+// Display all the words which are in dictionary order smaller than the string entered in a new line each. The output strings must be sorted.
+
+// Sample Input
+// cab
+// Sample Output
+// abc
+// acb
+// bac
+// bca
+// Explanation
+// The possible permutations of string "cab" are "abc" , "acb" ,"bac" , "bca" , "cab" and "cba" . Only four of them are lexicographically less than "cab". We print them in lexicographical order.
+
+
+
+
+import java.util.*;
+public class Main {
+    static String val ;
+    private static void sol(String inp, String ans,String str) {
+		// TODO Auto-generated method stub
+		if(inp.length()==0){
+		if(ans.compareTo(str) != 0 && ans.compareTo(str) < 0)
+			System.out.println(ans);
+			return;
+		}
+		for(int i = 0; i < inp.length(); i++) {
+			String bachiHuiString = inp.substring(0, i) + inp.substring(i + 1);
+			char nikalaFixed = inp.charAt(i);
+			
+			sol(bachiHuiString, ans + nikalaFixed,str);
+		}
+	}
+      
+    public static void main(String args[]) {
+        // Your Code Here
+        Scanner sc=new Scanner(System.in);
+        String str1 = sc.next();
+        char[] ar = str1.toCharArray();
+        Arrays.sort(ar);
+        String str2 = new String(ar);  
+        if(str2.length()<=25)
+            sol(str2, "" ,str1);
+    }
+}
