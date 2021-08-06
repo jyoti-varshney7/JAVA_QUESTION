3c3041e22be4ffe04a764db5eda3c5b2732a91c1
Jyoti Varshney <jyoti.varshney_cs18@gla.ac.in>
2021-08-06 12:37:59 +0530

    Create TreeRightView.java

---
+++ TreeRightView.java
@@ -0,0 +1,157 @@
+// Given a binary tree , print its nodes from root to bottom as visible from right side of tree
+
+// Input Format
+// Level order input for the binary tree will be given.
+
+// Constraints
+// No of nodes in the tree can be less than or equal to 10^7
+
+// Output Format
+// A single line containing space separated integers representing the right view of the tree
+
+// Sample Input
+// 1 2 3 4 5 -1 6 -1 -1 -1 -1 -1 -1
+// Sample Output
+// 1 3 6
+// Explanation
+// The tree looks like
+
+//              1
+//           /      \
+//        2           3
+//     /     \           \
+//    4       5           6
+// When viewed from the left , we would see the nodes 1,3 and 6.
+
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String args[]) throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String[] arr=br.readLine().split(" ");
+        BinaryTree bt=new BinaryTree(arr);
+        
+        bt.rightView();
+        
+    }
+}
+
+class BinaryTree
+{
+    private class Node
+    {
+        int data;
+        Node left,right;
+        Node(int data)
+        {
+            this.data=data;
+        }
+    }
+
+    Node root;
+
+    BinaryTree(String[] arr)
+    {
+        Queue<Node> queue=new LinkedList<Node>();
+        construct(arr,0,queue);
+    }
+
+    public void rightView()
+    {
+        rightView(this.root , 1);
+    }
+	int level = 0;
+   private void rightView(Node root, int currentLevel) {
+	   if (root == null) {
+            return;
+        }
+        
+        if (level < currentLevel && root.data > 0){
+            System.out.print(root.data + " ");    
+            level = currentLevel;
+        }
+        
+        if (root.left != null) {
+            rightView(root.right, currentLevel + 1);
+        } 
+
+        if (root.right != null) {
+            rightView(root.left, currentLevel + 1);
+        }
+	   
+	}
+
+
+    private void construct(String[] arr,int ind,Queue<Node> queue)
+    {
+        if(ind>=arr.length)
+        return;
+        if(queue.size()==0)
+        {
+            Node nn=new Node(Integer.parseInt(arr[ind]));
+            this.root=nn;
+            queue.add(nn);
+        }
+        else
+        {
+            Node parent=queue.peek();
+                if(parent.data!=-1){
+                if(parent.left==null)
+                {
+                    parent.left=new Node(Integer.parseInt(arr[ind]));
+                    queue.add(parent.left);
+                }
+                else
+                {
+                    if(parent.right==null)
+                    {
+                    parent.right=new Node(Integer.parseInt(arr[ind]));
+                    queue.add(parent.right);
+                    queue.poll();
+                    }
+               }
+               }
+               else
+               {
+                   queue.poll();
+                   ind--;
+               }
+        }
+        construct(arr,ind+1,queue);
+    }
+
+    public void display()
+    {
+        display_tree(this.root);
+    }
+
+    private void display_tree(Node root)
+    {
+        if(root==null)
+        return;
+        String str=root.data+"";
+        if(root.left!=null)
+        {
+            str=root.left.data+" <= "+str;
+        }
+        else
+        {
+            str="END <= "+str;
+        }
+
+        if(root.right!=null)
+        {
+            str=str+" => "+root.right.data;
+        }
+        else
+        {
+            str=str+" => END";
+        }
+        System.out.println(str);
+        display_tree(root.left);
+        display_tree(root.right);
+
+    }
+
+
+}
